0a52504053b57b3a4a0bf5e358b4ac3c076bfe41
Jorge González Aparicio <jorgegatrabajo@gmail.com>
2024-10-22 12:23:20 +0200

    Updated code
    More comments, and added instructions.

--- pcd_map_viewer/src/pcd_map_viewer.cpp
+++ pcd_map_viewer/src/pcd_map_viewer.cpp
@@ -3,19 +3,21 @@
 #include <pcl_ros/point_cloud.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+#include <iostream>
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv){
+    //Path to .pcd file.
+    cv::String map_path = "/home/grvc/catkin_ws/src/pcd_map_viewer/maps/scans_obstaculos.pcd";
     ros::init(argc, argv, "pcd_map_viewer");
     ros::NodeHandle nh;
+    std::cout << "Using default map. To change map, replace the following file, maintaining the name and location:\n /home/grvc/catkin_ws/src/pcd_map_viewer/maps/scans_obstaculos.pcd" << std::endl;
 
     // Publisher for point cloud
     ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud2>("pcd_topic", 1);
 
     // Load PCD file
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    if (pcl::io::loadPCDFile<pcl::PointXYZ>("/home/grvc/Escritorio/scans_bueno.pcd", *cloud) == -1)
-    {
+    if (pcl::io::loadPCDFile<pcl::PointXYZ>(map_path, *cloud) == -1){
         ROS_ERROR("Couldn't read the PCD file");
         return -1;
     }
@@ -26,8 +28,7 @@ int main(int argc, char** argv)
     output.header.frame_id = "map";
 
     ros::Rate loop_rate(1);
-    while (ros::ok())
-    {
+    while (ros::ok()){
         output.header.stamp = ros::Time::now();
         pub.publish(output);
         ros::spinOnce();
